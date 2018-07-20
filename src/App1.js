5e467d4073592c730207776da59bdbf77da33168
Abdur Rehman <abdur.rehman.officials@gmail.com>
2018-07-20 07:08:41 -0700

    this is the new class and omponent inputs i am adding

--- src/App1.js
+++ src/App1.js
@@ -23,5 +23,26 @@ class App1 extends Component {
     );
   }
 }
+class Inputs extends Component {
+  render() {
+    return (
+      <div className="Inputs">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="App-intro">
+          To get started, I have edited <code>src/App.js</code> and save to
+          reload.
+          <label>
+            Name:
+            <input type="text" name="name" required="true" />
+          </label>
+          <input type="submit" value="Submit" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App1;
