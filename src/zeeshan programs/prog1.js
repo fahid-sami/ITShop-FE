43f958d96952b8dedc5c643596c0ce5e6a88ae39
shanch0311 <ra794455@gmail.com>
2018-07-28 20:41:07 +0500

    Program1

---
+++ src/zeeshan programs/prog1.js
@@ -0,0 +1,23 @@
+<html>
+	<head>
+	
+	</head>
+	<body>
+		<script type="text/javascript">
+
+		var c = "12345";
+		var a="";
+		
+		for (i=c.length-1;i>=0;i--)
+		{
+
+			a = a + c[i];
+		}
+
+			c=a;
+
+			document.write(c);
+
+		</script>
+	</body>
+</html>
